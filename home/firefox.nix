09a0bdcc69f5e2cd16bc2935dae74930a69f7a92
bytemouse <bytemouse@posteo.org>
2023-10-12 21:34:39 +0200

    firefox update

--- home/firefox.nix
+++ home/firefox.nix
@@ -2,13 +2,16 @@
 {
   programs.firefox = lib.mkIf nixosConfig.setup.gui.desktop.enable {
     enable = true;
-    settings = {
-      # enable firefox-sync
-      "identity.fxaccounts.enabled" = true;
-      "browser.uidensity" = 1;
-      "browser.warnOnQuit" = false;
-      "browser.toolbars.bookmarks.visibility" = "never";
-      "browser.sessionstore.resume_from_crash" = false; # starts a new session, when quiting with ctrl+q
+    profiles."default" = {
+      settings = {
+        # enable firefox-sync
+        isDefault = true;
+        "identity.fxaccounts.enabled" = true;
+        "browser.uidensity" = 1;
+        "browser.warnOnQuit" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.sessionstore.resume_from_crash" = false; # starts a new session, when quiting with ctrl+q
+      };
     };
   };
 }
